c6a4793861f6a5fc47b3d37ddff04d55a0c80b0f
Dmitriy Korotkov <nupellot@gmail.com>
2024-02-12 16:41:06 +0300

    Начал переработку дизайна

--- index.js
+++ index.js
@@ -25,16 +25,36 @@ const db = new sqlite3.Database(db_name, err => {
     console.log(`Подключено к базе данных`);
 });
 
+
 // Запуск сервера на порту 3000
 app.listen(3000, () => {
     console.log("Сервер запущен: http://localhost:3000/");
 });
+
 
 // Обработка корневого маршрута
 app.get("/", (req, res) => {
     // Перенаправление на маршрут "/shifts"
     res.redirect("/shifts");
 });
+
+
+
+// Обработка GET-запроса для страницы создания записи
+app.get("/create", (req, res) => {
+    // SQL-запрос для выборки данных о типах процедур из базы данных
+    const sql = "SELECT * FROM procedure_types ORDER BY id";
+
+    // Выполнение запроса
+    db.all(sql, [], (err, procedure_types) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        // Рендеринг страницы создания записи и передача данных в представление
+        res.render("createShift", {model: {}, row: procedure_types});
+    });
+});
+
 
 // Обработка POST-запроса для создания записи
 app.post("/create", (req, res) => {
@@ -86,6 +106,8 @@ app.post("/create", (req, res) => {
     console.log(difficulty);
 });
 
+
+
 // Обработка GET-запроса для страницы со списком смен
 app.get("/shifts", (req, res) => {
     // SQL-запрос для выборки данных о сменах из базы данных
@@ -101,20 +123,67 @@ app.get("/shifts", (req, res) => {
     });
 });
 
-// Обработка GET-запроса для страницы создания записи
-app.get("/create", (req, res) => {
-    // SQL-запрос для выборки данных о типах процедур из базы данных
-    const sql = "SELECT * FROM procedure_types ORDER BY id";
+
+
+// Обработка GET-запроса для страницы редактирования записи
+app.get("/edit/:id", (req, res) => {
+    // Извлечение параметра ID из запроса
+    const id = req.params.id;
+
+    // SQL-запрос для выборки данных о конкретной записи по ID
+    const sql = "SELECT * FROM shifts WHERE id = ?";
 
     // Выполнение запроса
-    db.all(sql, [], (err, procedure_types) => {
+    db.get(sql, id, (err, row) => {
         if (err) {
             return console.error(err.message);
         }
-        // Рендеринг страницы создания записи и передача данных в представление
-        res.render("createShift", {model: {}, row: procedure_types});
+
+        // SQL-запрос для выборки данных о типах процедур из базы данных
+        const procedureTypesSQL = "SELECT * FROM procedure_types ORDER BY id";
+
+        // Выполнение запроса
+        db.all(procedureTypesSQL, [], (err, procedure_types) => {
+            if (err) {
+                return console.error(err.message);
+            }
+
+            // Рендеринг страницы редактирования записи и передача данных в представление
+            res.render("editShift", { model: row, row: procedure_types });
+        });
     });
 });
+
+// Обработка POST-запроса для обновления записи по ID
+app.post("/edit/:id", (req, res) => {
+    // Извлечение параметра ID из запроса
+    const id = req.params.id;
+
+    // SQL-запрос для обновления данных в записи по ID
+    const updateSQL = "UPDATE shifts SET procedures = ?, date = ?, difficulty = ? WHERE id = ?";
+
+    // Получение данных из тела запроса
+    let book = req.body.date;
+    let procedures = req.body.procedures.split(',');
+    let difficulty = 0;
+
+    // Расчет сложности процедур
+    for (let i = 1; i < procedures.length; i += 2) {
+        difficulty += Number(procedures[i]);
+    }
+
+    // Выполнение запроса на обновление записи
+    db.run(updateSQL, procedures.join(','), book, difficulty, id, err => {
+        if (err) {
+            return console.error(err.message);
+        }
+        // Перенаправление на маршрут "/shifts" после успешного обновления записи
+        res.redirect("/shifts");
+    });
+});
+
+
+
 
 // Обработка POST-запроса для удаления записи по ID
 app.post("/delete/:id", (req, res) => {
